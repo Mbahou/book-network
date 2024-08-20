ec764709fe691e68d2aa2d12f8c3a9bac03038dc
El Mehdi Bahou <mbahou777@gmail.com>
2024-08-20 02:38:20 +0100

    CI/CD backend pipline

--- book-network-api/src/main/java/com/bahou/book/config/BeansConfig.java
+++ book-network-api/src/main/java/com/bahou/book/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package com.bahou.book.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -26,6 +27,9 @@ import static org.springframework.http.HttpHeaders.*;
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+
+    @Value("${application.cors.origins:*}")
+    private List<String> allowedOrigins;
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
@@ -54,21 +58,10 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource  source =
                 new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config =  new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:8080"));
-        config.setAllowedHeaders(Arrays.asList(
-                ORIGIN,
-                CONTENT_TYPE,
-                ACCEPT,
-                AUTHORIZATION
-        ));
-        config.setAllowedMethods(Arrays.asList(
-                "GET",
-                "POST",
-                "DELETE",
-                "PUT",
-                "PATCH"
-        ));
+        //config.setAllowCredentials(true);
+        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedHeaders(Arrays.asList("*"));  // not recommended for production
+        config.setAllowedMethods(Arrays.asList("*"));  // not recommended for production
         source.registerCorsConfiguration("/**",config);
         return new CorsFilter(source);
     }
